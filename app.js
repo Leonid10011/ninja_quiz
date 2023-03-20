39c7170c988ff9326c54e24557d1f03b799d43e8
Leonid10011 <leonid.budkov@gmail.com>
2023-03-20 17:38:14 +0100

    Added score animation

--- app.js
+++ app.js
@@ -5,19 +5,25 @@ const anwserSpan = document.querySelector(".result");
 form.addEventListener("submit", e => {
     e.preventDefault();
     let score = 0;
-
     const userAnwers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
-
     // check anwers
     userAnwers.forEach((a,i) => {
         if(a === correctAnswers[i]){
                 score += 25;
             }
     });
-
     // show result
     scrollTo(0,0);
-    anwserSpan.children[0].children[0].children[0].textContent=`${score}%`;
+    
     anwserSpan.classList.remove("d-none");
 
+    let output = 0;
+    const timer = setInterval(() => {
+        anwserSpan.children[0].children[0].children[0].textContent=`${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        }else{
+            output += 1;
+        }
+    },10);
 });
